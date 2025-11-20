05255241dbcabf8c82ddf72a1a444a66f3169298
Rami Izhiman <118925549+IzhimanRami@users.noreply.github.com>
2025-11-20 18:05:29 +0400

    Update Files.jsx

--- frontend/src/pages/Files.jsx
+++ frontend/src/pages/Files.jsx
@@ -1,2 +1,11 @@
+import React from "react";
 import DriveIngestButton from "../components/DriveIngestButton";
-<DriveIngestButton />
+
+export default function FilesPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold mb-4">Files</h1>
+      <DriveIngestButton />
+    </div>
+  );
+}
